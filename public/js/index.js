7277486aa842fa02688e29f7ee2d2a1645a8dc4a
Andrew Burke <Drewburke39@gmail.com>
2021-04-18 17:17:17 -0600

    init commit

---
+++ public/js/index.js
@@ -0,0 +1,145 @@
+const actionBtn = document.getElementById("action-button");
+// new item
+var $noteTitle = $(".noteTitle");
+var $noteText = $(".noteTextarea");
+var $saveNoteBtn = $(".saveNote");
+var $newNoteBtn = $(".new-note");
+var $noteList = $(".list-container .list-group");
+
+// activeNote is used to keep track of the note in the textarea
+var currentNote = {};
+
+// A function for getting all notes from the db
+var getNotes = function () {
+    return $.ajax({
+        url: "/api/notes",
+        method: "GET"
+    });
+};
+
+// A function for saving a note to the db
+var saveNote = function (note) {
+    return $.ajax({
+        url: "/api/notes",
+        data: note,
+        method: "POST"
+    });
+};
+
+// A function for deleting a note from the db
+var deleteNote = function (id) {
+    return $.ajax({
+        url: "api/notes/" + id,
+        method: "DELETE"
+    });
+};
+
+// If there is an activeNote, display it, otherwise render empty inputs
+var renderCurrentNote = function () {
+    $saveNoteBtn.hide();
+
+    if (currentNote.id) {
+        $noteTitle.attr("readonly", true);
+        $noteText.attr("readonly", true);
+        $noteTitle.val(currentNote.title);
+        $noteText.val(currentNote.text);
+    } else {
+        $noteTitle.attr("readonly", false);
+        $noteText.attr("readonly", false);
+        $noteTitle.val("");
+        $noteText.val("");
+    }
+};
+
+// Get the note data from the inputs, save it to the db and update the view
+var handleNoteSave = function () {
+    var newNote = {
+        title: $noteTitle.val(),
+        text: $noteText.val()
+    };
+
+    saveNote(newNote).then(function (data) {
+        getAndRender();
+        renderCurrentNote();
+    });
+};
+
+// Delete the clicked note
+var handleNoteDelete = function (event) {
+    // prevents the click listener for the list from being called when the button inside of it is clicked
+    event.stopPropagation();
+
+    var note = $(this)
+        .parent(".list-group-item")
+        .data();
+
+    if (currentNote.id === note.id) {
+        currentNote = {};
+    }
+
+    deleteNote(note.id).then(function () {
+        getAndRender();
+        renderCurrentNote();
+    });
+};
+
+// Sets the activeNote and displays it
+var handleNoteView = function () {
+    currentNote = $(this).data();
+    renderCurrentNote();
+};
+
+// Sets the activeNote to and empty object and allows the user to enter a new note
+var handleNewNoteView = function () {
+    currentNote = {};
+    renderCurrentNote();
+};
+
+// If a note's title or text are empty, hide the save button
+// Or else show it
+var handleRenderSaveBtn = function () {
+    if (!$noteTitle.val().trim() || !$noteText.val().trim()) {
+        $saveNoteBtn.hide();
+    } else {
+        $saveNoteBtn.show();
+    }
+};
+
+// Render's the list of note titles
+var renderNoteList = function (notes) {
+    $noteList.empty();
+
+    var noteListItems = [];
+
+    for (var i = 0; i < notes.length; i++) {
+        var note = notes[i];
+
+        var $li = $("<li class='list-group-item'>").data(note);
+        var $span = $("<span>").text(note.title);
+        var $delBtn = $(
+            "<i class='fas fa-trash-alt float-right text-danger delete-note'>"
+        );
+
+        $li.append($span, $delBtn);
+        noteListItems.push($li);
+    }
+
+    $noteList.append(noteListItems);
+};
+
+// Gets notes from the db and renders them to the sidebar
+var getAndRender = function () {
+    return getNotes().then(function (data) {
+        renderNoteList(data);
+    });
+};
+
+$saveNoteBtn.on("click", handleNoteSave);
+$noteList.on("click", ".list-group-item", handleNoteView);
+$newNoteBtn.on("click", handleNewNoteView);
+$noteList.on("click", ".delete-note", handleNoteDelete);
+$noteTitle.on("keyup", handleRenderSaveBtn);
+$noteText.on("keyup", handleRenderSaveBtn);
+
+// Gets and renders the initial list of notes
+getAndRender();
--- server.js
+++ server.js
@@ -1,15 +1,17 @@
-const express = require('express');
+const express = require("express");
+// const path = require("path");
 const PORT = process.env.PORT || 8080;
 
 const app = express();
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
 app.use(express.static('public'));
 
-require('./routes/apiRoutes');
-require('./routes/htmlRoutes');
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
 
 app.listen(PORT, () => {
-    console.log(`Server is listening on port: ${PORT}`)
+    console.log(`Server is listening port: ${PORT}`)
 })
