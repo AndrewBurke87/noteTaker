82f87e128d3b1884a681423683cfee37cbc87135
Andrew Burke <Drewburke39@gmail.com>
2021-04-22 16:49:14 -0600

    It works, It's alive lol

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,60 +1,38 @@
 const fs = require("fs");
+const path = require('path');
+const uniqid = require('uniqid');
+let noteList = require('../db/db.json');
 
+module.exports = (app) => {
 
-module.exports = function (app) {
-    let notes = require("../db/db.json")
+    app.post('/api/notes', (req, res) => {
 
-    app.get("/api/notes", (req, res) => {
-        return res.json(notes)
-    })
-
-    app.get("/api/notes/:id", (req, res) => {
-        const id = req.params.id;
-        let found;
-        notes.forEach(n => {
-            if (id == n.id) {
-                found = n;
-                return res.json(n)
-            }
-        })
-        return res.json(false)
-    })
-
-
-    app.post("/api/notes", (req, res) => {
-        const newNote = req.body;
-        if (notes.length === 0) {
-            newNote.id = 1
-        } else {
-            newNote.id = (notes[notes.length - 1].id + 1);
-        }
-        notes.push(newNote);
-        let jsonNotes = JSON.stringify(notes)
-        fs.writeFile("./db/db.json", jsonNotes, function (err) {
-            if (err) {
-                return console.log(err);
-            }
-            console.log("Success!");
-        })
-        res.json(true)
-    })
-
-    app.delete("/api/notes/:id", (req, res) => {
-        const id = req.params.id;
-        notes.forEach((n, index) => {
-            if (id == n.id) {
-                notes.splice(index, 1)
-                const notesCopy = notes.slice();
-                let jsonNotes = JSON.stringify(notesCopy)
-                fs.writeFile("./db/db.json", jsonNotes, function (err) {
-                    if (err) {
-                        return console.log(err);
-                    }
-                    console.log("Success!");
-                })
-
-            }
-        })
+        let newNote = req.body;
+        newNote.id = uniqid();
+        noteList.push(newNote);
         res.json(true);
-    })
-}
+        updateData();
+
+
+    });
+
+    app.get("/api/notes", function (req, res) {
+        res.json(noteList);
+    });
+
+
+    app.delete('/api/notes/:id', function (req, res) {
+        noteList = noteList.filter(({ id }) =>
+            id !== req.params.id
+        );
+        updateData();
+        res.json(noteList);
+
+    });
+
+    function updateData() {
+        fs.writeFileSync("./db/db.json", JSON.stringify(noteList, '\t'), (err) => {
+            if (err) throw err;
+        });
+    }
+};
--- server.js
+++ server.js
@@ -1,17 +1,26 @@
+
+// Require Dependencies
+
 const express = require("express");
-// const path = require("path");
+const fs = require("fs");
+const path = require('path');
+
+// Initialize express app
+const app = express();
 const PORT = process.env.PORT || 8080;
 
-const app = express();
-
-app.use(express.urlencoded({ extended: true }))
+// Setup data parsing
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+//Require routes files
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
 
 
-app.listen(PORT, () => {
-    console.log(`Server is listening port: ${PORT}`)
-})
+
+// Setup listener
+app.listen(PORT, function () {
+    console.log("App listening on PORT: " + PORT);
+});
